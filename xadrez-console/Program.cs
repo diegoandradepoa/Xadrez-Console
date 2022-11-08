956e3ef67ca3bef7b0cc731d06ccd9cebcbd9577
Diego Cruz <diegoandradecruz15@gmail.com>
2022-11-07 22:51:08 -0300

    Comentario

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,7 +7,7 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
+            Tabuleiro tab = new Tabuleiro(8,8);  // Teste
 
             Console.ReadLine();
         }
