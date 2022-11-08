3a96938406a50d43e45e280d59388e0c74b50fa7
Diego Cruz <diegoandradecruz15@gmail.com>
2022-11-07 22:50:25 -0300

    Add project files.

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    internal class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) // Construtor com argumentos
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString() // Sobreposição para exibir na tela
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
